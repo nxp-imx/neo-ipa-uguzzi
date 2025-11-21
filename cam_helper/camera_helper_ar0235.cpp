bd9e9f4eb24b41bfac9c2e8c764401341513e438
Antoine Bouyer <antoine.bouyer@nxp.com>
2025-11-21 11:34:29 +0100

    ipa: neo: Add support for ar0235 sensor
    Added calibration file and helper to support Onsemi AR0235 monochrome
    sensor. Copy calibration file from uncalibrated.yaml, and remove awb which
    is not relevant for monochrome sensor.
    
    Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>

---
+++ cam_helper/camera_helper_ar0235.cpp
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * camera_helper_ar0235.c
+ * Helper class that performs sensor-specific parameter computations
+ * for Onsemi AR0235 sensor
+ * Copyright 2025 NXP
+ */
+
+#include <linux/v4l2-controls.h>
+
+#include "camera_helper.h"
+
+namespace libcamera {
+
+namespace nxp {
+
+class CameraHelperAr0235 : public CameraHelper
+{
+public:
+	CameraHelperAr0235()
+	{
+		/* gainType_ / gainConstants_ are unused */
+
+		/* Adapt the default delayedControls for the ar0235 custom controls */
+		attributes_.delayedControlParams = {
+			{ V4L2_CID_ANALOGUE_GAIN, { 3, false } },
+			{ V4L2_CID_EXPOSURE, { 3, false } },
+		};
+	}
+
+	uint32_t gainCode(double gain) const override;
+	double gain(uint32_t gainCode) const override;
+
+private:
+	static constexpr double kGainMin = 1.1875;
+	static constexpr double kGainMax = 8.0;
+
+	static constexpr uint32_t kCodeMin = 3;
+	static constexpr uint32_t kCodeMax = 46;
+};
+
+uint32_t CameraHelperAr0235::gainCode(double gain) const
+{
+	gain = std::clamp(gain, kGainMin, kGainMax);
+
+	if (gain < 2.0)
+		return (gain - 1.0) / 0.0625;
+	else if (gain < 4.0)
+		return 16 + (gain - 2.0) / 0.125;
+	else
+		return 32 + (gain - 4.0) / 0.25;
+}
+
+double CameraHelperAr0235::gain(uint32_t gainCode) const
+{
+	gainCode = std::clamp(gainCode, kCodeMin, kCodeMax);
+
+	if (gainCode < 16)
+		return 1.0 + gainCode * 0.0625;
+	else if (gainCode < 32)
+		return 2.0 + gainCode * 0.125;
+	else
+		return 4.0 + gainCode * 0.25;
+}
+
+REGISTER_CAMERA_HELPER("ar0235", CameraHelperAr0235)
+
+} /* namespace nxp */
+
+} /* namespace libcamera */
