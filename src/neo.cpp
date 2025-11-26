be3b546578209d2ed562c1da97d8614256070130
Julien Vuillaumier <julien.vuillaumier@nxp.com>
2025-11-26 17:37:15 +0100

    neo: ipa: Report the AutoFocus statistics
    Implement the AutoFocus statistics reporting expected by uguzzi, that
    consists in the output of the ISP AF block as well as DRC local
    statistics.
    AutoFocus parameters configuration is already present in the
    implementation.
    
    Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

--- src/neo.cpp
+++ src/neo.cpp
@@ -134,6 +134,10 @@ private:
 				   const neoisp_ctemp_reg_stats_s *ctempRegs,
 				   const neoisp_ctemp_mem_stats_s *ctempMems,
 				   awb_statistics_data_hw_t *outAwbData);
+	void prepareUguzziAfStats(const vpipe_settings_hw_t *cfg,
+				  const neoisp_af_reg_stats_s *afRegs,
+				  const neoisp_drc_mem_stats_s *drcMems,
+				  af_statistics_data_hw_t *outAfData);
 	void prepareUguzziStats(const NxpNeoStats *stats);
 
 	int processUguzzi(uguzzi_sensor_data_pkg_t *sensorDataPkg,
@@ -205,6 +209,7 @@ private:
 	ae_statistics_data_hw_t mAgblbceStats[UGUZZI_CAMERA_CNT];
 	awb_statistics_data_hw_t mAwbStatsData[UGUZZI_CAMERA_CNT];
 	imx9x_isp_awb_stats_type_t mAwbStatType[UGUZZI_CAMERA_CNT];
+	af_statistics_data_hw_t mAfStatsData[UGUZZI_CAMERA_CNT];
 
 	/* uguzzi_process outputs */
 	uguzzi_sensor_settings_pkg_t mSensorSettingsPkg;
@@ -322,6 +327,7 @@ int IPANxpNeo::initializeUguzzi(Size outputSize)
 	mStatsDataPkg.channel[channel_].p_ae_hist = &mAeHistData[channel_];
 	mStatsDataPkg.channel[channel_].p_awb_stats = &mAwbStatsData[channel_];
 	mStatsDataPkg.channel[channel_].p_awb_sw_stats = nullptr;
+	mStatsDataPkg.channel[channel_].p_af_stats = &mAfStatsData[channel_];
 	mSensorSettingsPkg.channel[channel_] = &mSensorSettings[channel_];
 	mIspSettingsPkg.isp_config[channel_] = &mIspSettings[channel_];
 
@@ -837,6 +843,35 @@ void IPANxpNeo::prepareUguzziAwbStats(imx9x_isp_awb_stats_type_t statType,
 	}
 }
 
+void IPANxpNeo::prepareUguzziAfStats(const vpipe_settings_hw_t *cfg,
+				     const neoisp_af_reg_stats_s *afRegs,
+				     const neoisp_drc_mem_stats_s *drcMems,
+				     af_statistics_data_hw_t *outAfData)
+{
+	/* Populate CDAF statistics. */
+	uint32_t *filter0Sums = &outAfData->af_rois_stat.filter0_sums[0];
+	uint32_t *filter1Sums = &outAfData->af_rois_stat.filter1_sums[0];
+	for (unsigned int i = 0; i < AUTOFOCUS_ROI_CNT; i++) {
+		filter0Sums[i] = afRegs->rois[i].sum0;
+		filter1Sums[i] = afRegs->rois[i].sum1;
+	}
+
+	/* Report AF ROI definitions. */
+	const imx9x_isp_autofocus_roi_cfg_t *roisConfig =
+		&cfg->isp_cfg_params[0].autofocus.rois_config[0];
+	imx9x_isp_autofocus_roi_cfg_t *roisOut = &outAfData->rois_config[0];
+	std::copy(roisConfig, roisConfig + AUTOFOCUS_ROI_CNT, roisOut);
+
+	/* Populate DRC local 32x32 grid configuration and statistics. */
+	outAfData->block_stats = drcMems->drc_local_sum;
+	const imx9x_isp_drc_local_tonemap_ctrl_cfg_t *localDrcConfig =
+		 &cfg->isp_cfg_params[0].drc_local_tonemap_ctrl;
+	outAfData->block_width = localDrcConfig->block_size_x;
+	outAfData->block_height = localDrcConfig->block_size_y;
+	outAfData->block_cnt_horz = 32;
+	outAfData->block_cnt_vert = 32;
+}
+
 void IPANxpNeo::prepareUguzziStats(const NxpNeoStats *stats)
 {
 	auto rgbirStats = stats->block<BlockStatsType::MRgbIr>();
@@ -857,6 +892,12 @@ void IPANxpNeo::prepareUguzziStats(const NxpNeoStats *stats)
 	mAgblbceStats[channel_].global_hist_roi0 = drcMemStats->drc_global_hist_roi0;
 	mAgblbceStats[channel_].global_hist_roi1 = drcMemStats->drc_global_hist_roi1;
 	mAgblbceStats[channel_].local_stats = drcMemStats->drc_local_sum;
+
+	auto afStats = stats->block<BlockStatsType::RAf>();
+	prepareUguzziAfStats(&mIspSettings[channel_],
+			     afStats.stats(),
+			     drcMemStats.stats(),
+			     &mAfStatsData[channel_]);
 
 	mStatsDataPkg.channel[channel_].stats_type = HW_STATS;
 	mStatsDataPkg.channel[channel_].valid = 1;
