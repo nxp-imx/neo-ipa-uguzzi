b41918556149c2ad7489c5c634c9788a533205e6
Julien Vuillaumier <julien.vuillaumier@nxp.com>
2025-11-26 15:06:54 +0100

    ipa: neo: Add camhelper for community sensors
    Addition of camhelpers for sensors commonly used by community:
    - Sony imx219 (RPi camera module 2)
    - Sony imx519 (Arducam imx519 module)
    - Sony imx708 (RPi camera module 3)
    
    Signed-off-by: Julien Vuillaumier <julien.vuillaumier@nxp.com>

---
+++ cam_helper/camera_helper_imx708.cpp
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * camera_helper_imx708.c
+ * Helper class that performs sensor-specific parameter computations
+ * for Sony imx708 sensor
+ * Copyright 2025 NXP
+ */
+
+#include "camera_helper.h"
+
+namespace libcamera {
+
+namespace nxp {
+
+class CameraHelperImx708 : public CameraHelper
+{
+public:
+	uint32_t gainCode(double gain) const override;
+	double gain(uint32_t gainCode) const override;
+};
+
+/* Gain conversions come from RPi cam_helper_imx708.cpp implementation */
+uint32_t CameraHelperImx708::gainCode(double gain) const
+{
+	return static_cast<uint32_t>(1024 - 1024 / gain);
+}
+
+double CameraHelperImx708::gain(uint32_t gainCode) const
+{
+	return 1024.0 / (1024 - gainCode);
+}
+
+REGISTER_CAMERA_HELPER("imx708", CameraHelperImx708)
+
+} /* namespace nxp */
+
+} /* namespace libcamera */
