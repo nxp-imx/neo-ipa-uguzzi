ea0724085b04298dcd9ef088f4ba2dac5a2899a6
Celine Laurencin <celine.laurencin@nxp.com>
2026-01-12 15:16:33 +0100

    ipa: neo: Update copyright to 2026
    The copyright from neo.cpp is updated with 2026.
    
    Signed-off-by: Celine Laurencin <celine.laurencin@nxp.com>
    (cherry picked from commit 206c9336998edc76888d8cb08a85baa1a563e74c)

--- src/neo.cpp
+++ src/neo.cpp
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 /*
  * neo.cpp - NXP NEO Image Processing Algorithms
- * Copyright 2024-2025 NXP
+ * Copyright 2024-2026 NXP
  */
 
 #include <sstream>
