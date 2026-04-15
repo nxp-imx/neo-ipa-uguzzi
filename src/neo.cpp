nxp-imx/neo-ipa-uguzzi

commit 6f7247906794a4d8274d3e5ee490e97b9809bf38

ipa: neo: Fix frame type and error handling
- Change the frame parameter type from unsigned int
  to uint32_t
- Replace ASSERT with proper error return to handle
  invalid buffer

Signed-off-by: Alexi Birlinger <alexi.birlinger@nxp.com>

src/neo.cpp
@@ -186,11 +186,11 @@ private:
 	int processUguzzi(unsigned int channel);
 
 	void setInitialControls();
-	void setControls(unsigned int frame);
+	void setControls(uint32_t frame);
 	bool libcameraCfa2UguzziBayerPattern(uint32_t cfa,
 					     uguzzi_cam_info_cfa_t *pattern);
 	std::string controlListToString(const ControlList *ctrls) const;
-	std::string logSensorParams(const unsigned int frame,
+	std::string logSensorParams(const uint32_t frame,
 				    const ControlList *ctrlsApplied,
 				    const ControlList *ctrlsToApply) const;
 
@@ -1193,7 +1193,7 @@ void IPANxpNeo::setInitialControls()
 	setControls(0);
 }
 
-void IPANxpNeo::setControls(unsigned int frame)
+void IPANxpNeo::setControls(uint32_t frame)
 {
 	/*
 	 * Send controls if:
@@ -1633,7 +1633,12 @@ void IPANxpNeo::computeParams(const uint32_t frame, const IPAContextType context
 	auto paramsIter = bufferIds.find(IPABufferTypeParams);
 	unsigned int paramsBufferId =
 		paramsIter != bufferIds.end() ? paramsIter->second : 0;
-	ASSERT(buffers_.count(paramsBufferId));
+	if (!mappedBuffers_.count(paramsBufferId)) {
+		LOG(NxpNeoIPA, Error)
+			<< "Parameters buffer " << paramsBufferId
+			<< " not mapped for frame " << frame;
+		return;
+	}
 	NxpNeoParams params(context_.hw.apiVersion,
 			    buffers_.at(paramsBufferId).planes()[0]);
 
@@ -1654,7 +1659,12 @@ void IPANxpNeo::processStats(const uint32_t frame, const IPAContextType context,
 	auto statsIter = bufferIds.find(IPABufferTypeStats);
 	unsigned int statsBufferId =
 		statsIter != bufferIds.end() ? statsIter->second : 0;
-	ASSERT(buffers_.count(statsBufferId));
+	if (!mappedBuffers_.count(statsBufferId)) {
+		LOG(NxpNeoIPA, Error)
+			<< "Statistics buffer " << statsBufferId
+			<< " not mapped for frame " << frame;
+		return;
+	}
 
 	const NxpNeoStats stats(context_.hw.apiVersion,
 				buffers_.at(statsBufferId).planes()[0]);
@@ -1945,7 +1955,7 @@ std::string IPANxpNeo::controlListToString(const ControlList *ctrls) const
 	return log.str();
 }
 
-std::string IPANxpNeo::logSensorParams(const unsigned int frame,
+std::string IPANxpNeo::logSensorParams(const uint32_t frame,
 				       const ControlList *ctrlsApplied,
 				       const ControlList *ctrlsToApply) const
 {

commit 57baaddffb1c1480816bd890b14b29c9deab36e3

ipa: neo: Fix build error due to wrongly introduced typos
Fixes: 6f724790679 ("ipa: neo: Fix frame type and error handling")
- Updated mappedBuffers_ to buffers_
- Updated NxpNeoIPA to NxpNeoUguzziIPA

Signed-off-by: Alexi Birlinger <alexi.birlinger@nxp.com>

src/neo.cpp
@@ -1633,8 +1633,8 @@ void IPANxpNeo::computeParams(const uint32_t frame, const IPAContextType context
 	auto paramsIter = bufferIds.find(IPABufferTypeParams);
 	unsigned int paramsBufferId =
 		paramsIter != bufferIds.end() ? paramsIter->second : 0;
-	if (!mappedBuffers_.count(paramsBufferId)) {
-		LOG(NxpNeoIPA, Error)
+	if (!buffers_.count(paramsBufferId)) {
+		LOG(NxpNeoUguzziIPA, Error)
 			<< "Parameters buffer " << paramsBufferId
 			<< " not mapped for frame " << frame;
 		return;
@@ -1659,8 +1659,8 @@ void IPANxpNeo::processStats(const uint32_t frame, const IPAContextType context,
 	auto statsIter = bufferIds.find(IPABufferTypeStats);
 	unsigned int statsBufferId =
 		statsIter != bufferIds.end() ? statsIter->second : 0;
-	if (!mappedBuffers_.count(statsBufferId)) {
-		LOG(NxpNeoIPA, Error)
+	if (!buffers_.count(statsBufferId)) {
+		LOG(NxpNeoUguzziIPA, Error)
 			<< "Statistics buffer " << statsBufferId
 			<< " not mapped for frame " << frame;
 		return;